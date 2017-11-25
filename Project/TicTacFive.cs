89fb494b984dab19e8c76969f3013b206d0f4e64
icygub <jchubbs95@gmail.com>
2017-11-25 14:52:43 -0700

    added unit tests

---
+++ Project/TicTacFive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project {
+    public class TicTacFive {
+
+        private char[,] _board;
+        
+        public TicTacFive(int cols, int rows) {
+            _board = new char[cols, rows];
+            for(int col = 0; col < cols; col++) {
+                for(int row = 0; row < rows; row++) {
+                    _board[col, row] = ' ';
+                }
+            }
+        }
+
+        public char[,] Board {
+            get { return _board; }
+            private set { _board = value; }
+        }
+
+        public bool FieldIsTaken(int col, int row) {
+            return !Board[col, row].Equals(' ');
+        }
+
+        public bool PlaceChar(char c, int col, int row) {
+            if(! FieldIsTaken(col,row)) {
+                Board[col, row] = c;
+                return true;
+            }
+            return false;
+        }
+
+        
+    }
+}
---
+++ UnitTests/TicTacFiveTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project;
+
+namespace UnitTests {
+    [TestClass]
+    public class BoardTest {
+        [TestMethod]
+        public void FieldIsTaken_Should_Return_True_When_A_Field_Is_Taken() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('X', 1, 4);          
+            bool isEmpty = tic.FieldIsTaken(1, 4);
+            Assert.IsTrue(isEmpty);
+        }
+
+        [TestMethod]
+        public void FieldIsTaken_Should_Return_False_When_The_Field_Is_Not_Taken() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('X', 1, 4);
+            bool isEmpty = tic.FieldIsTaken(3, 3);
+            Assert.IsFalse(isEmpty);
+        }
+
+        [TestMethod]
+        public void PlaceChar_Should_Return_True_When_Char_Is_Placed_In_Empty_Field() {
+            TicTacFive tic = new TicTacFive(5, 5);           
+            Assert.IsTrue(tic.PlaceChar('X', 1, 4));
+        }
+
+        [TestMethod]
+        public void PlaceChar_Should_Place_Char_In_Correct_Location() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('X', 1, 4);
+            Assert.IsTrue(tic.Board[1, 4].Equals('X'));
+        }
+
+        [TestMethod]
+        public void PlaceChar_Should_Return_False_When_Field_Is_Taken() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('O', 1, 4);
+            Assert.IsFalse(tic.PlaceChar('X', 1, 4));
+        }
+    }
+}
