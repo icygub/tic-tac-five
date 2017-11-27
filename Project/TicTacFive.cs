ae2f87136aa08d4d71af8ab1a31a606d84ba652e
icygub <jchubbs95@gmail.com>
2017-11-26 22:09:14 -0700

    added more unit tests

--- Project/TicTacFive.cs
+++ Project/TicTacFive.cs
@@ -35,6 +35,77 @@ namespace Project {
             return false;
         }
 
-        
+        public bool CheckVerticals(char c) {
+            bool win = false;
+            for(int col = 0; col < Board.GetLength(0); col++) { //Board.GetLength(0) is number of cols in Board
+                for(int row = 0; row < Board.GetLength(1); row++) {
+                    if(Board[col,row] != c) {
+                        break;
+                    }
+                    else if(row == Board.GetLength(1) - 1) { 
+                        win = true;
+                    }
+                }
+                if(win == true) {
+                    break;
+                }
+            }
+            return win;
+        }
+
+        public bool CheckHorizontals(char c) {
+            bool win = false;
+            for(int row = 0; row < Board.GetLength(1); row++) { //Board.GetLength(1) is number of rows in Board
+                for (int col = 0; col < Board.GetLength(0); col++) {
+                    if(Board[col,row] != c) {
+                        break;
+                    }
+                    //if last col char matches 
+                    else if(col == Board.GetLength(0) - 1) {
+                        win = true;
+                    }
+                }
+                if(win == true) {
+                    break;
+                }
+            }
+            return win;
+        }
+
+        public bool CheckDiagonalUpRight(char c) {
+            bool win = false;
+            int col = 0;
+            // starts at [0,max] and ends at [max,0]
+            for (int row = Board.GetLength(1) - 1; row >= 0; row--) {
+                if (Board[col, row] != c) {
+                    break;
+                }
+                else if (row == 0) {
+                    win = true;
+                }
+                else {
+                    col++;
+                }
+            }
+            return win;
+        }
+
+        public bool CheckDiagonalDownRight(char c) {
+            bool win = false;
+            int col = 0;
+            // starts at [0,0] and ends at [max,max]
+            for(int row = 0; row < Board.GetLength(1); row++) {
+                if(Board[col,row] != c) {
+                    break;
+                }
+                else if(row == Board.GetLength(1) - 1) {
+                    win = true;
+                }
+                else {
+                    col++;
+                }
+            }
+            return win;
+        }
     }
 }
--- UnitTests/TicTacFiveTest.cs
+++ UnitTests/TicTacFiveTest.cs
@@ -40,5 +40,71 @@ namespace UnitTests {
             tic.PlaceChar('O', 1, 4);
             Assert.IsFalse(tic.PlaceChar('X', 1, 4));
         }
+
+        [TestMethod]
+        public void CheckVerticals_Should_Return_True_When_There_Are_Consecutive_Chars_In_Same_Column() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('X', 0, 0);
+            tic.PlaceChar('X', 0, 1);
+            tic.PlaceChar('X', 0, 2);
+            tic.PlaceChar('X', 0, 3);
+            tic.PlaceChar('X', 0, 4);
+            Assert.IsTrue(tic.CheckVerticals('X'));
+            tic.PlaceChar('O', 4, 0);
+            tic.PlaceChar('O', 4, 1);
+            tic.PlaceChar('O', 4, 2);
+            tic.PlaceChar('O', 4, 3);
+            tic.PlaceChar('O', 4, 4);
+            Assert.IsTrue(tic.CheckVerticals('O'));
+        }
+
+        [TestMethod]
+        public void CheckHorizontals_Should_Return_True_When_There_Are_Consecutive_Charss() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('X', 0, 0);
+            tic.PlaceChar('X', 1, 0);
+            tic.PlaceChar('X', 2, 0);
+            tic.PlaceChar('X', 3, 0);
+            tic.PlaceChar('X', 4, 0);
+            Assert.IsTrue(tic.CheckHorizontals('X'));
+            tic.PlaceChar('O', 0, 4);
+            tic.PlaceChar('O', 1, 4);
+            tic.PlaceChar('O', 2, 4);
+            tic.PlaceChar('O', 3, 4);
+            tic.PlaceChar('O', 4, 4);
+            Assert.IsTrue(tic.CheckHorizontals('O'));
+
+        }
+
+        [TestMethod]
+        public void CheckDiagonalDownRight_Should_Return_True_When_There_Are_Consecutive_Chars() {
+            TicTacFive tac = new TicTacFive(5, 5);
+            tac.PlaceChar('O', 0, 0);
+            tac.PlaceChar('O', 1, 1);
+            tac.PlaceChar('O', 2, 2);
+            tac.PlaceChar('O', 3, 3);
+            tac.PlaceChar('O', 4, 4);
+            Assert.IsTrue(tac.CheckDiagonalDownRight('O'));
+        }
+
+        [TestMethod]
+        public void CheckDiagonalUpRight_Should_Return_True_When_There_Are_Consecutive_Chars() {
+            TicTacFive tic = new TicTacFive(5, 5);
+            tic.PlaceChar('X', 0, 4);
+            tic.PlaceChar('X', 1, 3);
+            tic.PlaceChar('X', 2, 2);
+            tic.PlaceChar('X', 3, 1);
+            tic.PlaceChar('X', 4, 0);
+            Assert.IsTrue(tic.CheckDiagonalUpRight('X'));
+        }
+
+        [TestMethod]
+        public void GetLengths() {
+            TicTacFive tic = new TicTacFive(5, 4);
+            tic.PlaceChar('X', 0, 0);
+            Assert.IsTrue(tic.Board[0, 0].Equals('X'));
+            tic.PlaceChar('O', 0, 1);
+            Assert.IsTrue(tic.Board[0, 1].Equals('O'));
+        }
     }
 }
