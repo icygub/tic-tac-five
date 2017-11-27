2d59f8af0530dfe0e072770676057c21e87e9277
icygub <jchubbs95@gmail.com>
2017-11-27 13:56:12 -0700

    moved Game code to GameBoard

---
+++ Project/GameBoard.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project {
+    public class GameBoard {
+
+        private Piece[,] _board;
+
+        public GameBoard(int cols, int rows) {
+            _board = new Piece[cols, rows];
+            for (int col = 0; col < cols; col++) {
+                for (int row = 0; row < rows; row++) {
+                    _board[col, row] = Piece.EMPTY;
+                }
+            }
+        }
+
+        public Piece[,] Board {
+            get { return _board; }
+            private set { _board = value; }
+        }
+
+        public bool FieldIsTaken(int col, int row) {
+            return !Board[col, row].Equals(Piece.EMPTY);
+        }
+
+        public bool PlaceChar(Piece p, int col, int row) {
+            if (!FieldIsTaken(col, row)) {
+                Board[col, row] = p;
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckVerticals(Piece p) {
+            bool win = false;
+            for (int col = 0; col < Board.GetLength(0); col++) { //Board.GetLength(0) is number of cols in Board
+                for (int row = 0; row < Board.GetLength(1); row++) {
+                    if (Board[col, row] != p) {
+                        break;
+                    }
+                    else if (row == Board.GetLength(1) - 1) {
+                        win = true;
+                    }
+                }
+                if (win == true) {
+                    break;
+                }
+            }
+            return win;
+        }
+
+        public bool CheckHorizontals(Piece p) {
+            bool win = false;
+            for (int row = 0; row < Board.GetLength(1); row++) { //Board.GetLength(1) is number of rows in Board
+                for (int col = 0; col < Board.GetLength(0); col++) {
+                    if (Board[col, row] != p) {
+                        break;
+                    }
+                    //if last col char matches 
+                    else if (col == Board.GetLength(0) - 1) {
+                        win = true;
+                    }
+                }
+                if (win == true) {
+                    break;
+                }
+            }
+            return win;
+        }
+
+        public bool CheckDiagonalUpRight(Piece p) {
+            bool win = false;
+            int col = 0;
+            // starts at [0,max] and ends at [max,0]
+            for (int row = Board.GetLength(1) - 1; row >= 0; row--) {
+                if (Board[col, row] != p) {
+                    break;
+                }
+                else if (row == 0) {
+                    win = true;
+                }
+                else {
+                    col++;
+                }
+            }
+            return win;
+        }
+
+        public bool CheckDiagonalDownRight(Piece p) {
+            bool win = false;
+            int col = 0;
+            // starts at [0,0] and ends at [max,max]
+            for (int row = 0; row < Board.GetLength(1); row++) {
+                if (Board[col, row] != p) {
+                    break;
+                }
+                else if (row == Board.GetLength(1) - 1) {
+                    win = true;
+                }
+                else {
+                    col++;
+                }
+            }
+            return win;
+        }
+
+        public enum Piece {
+            EMPTY, X, O
+        }
+    }
+}
--- UnitTests/GameBoardTest.cs
+++ UnitTests/GameBoardTest.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Project;
-using static Project.Game;
+using static Project.GameBoard;
 
 namespace UnitTests {
     [TestClass]
-    public class GameTest {
+    public class GameBoardTest {
         [TestMethod]
         public void FieldIsTaken_Should_Return_True_When_A_Field_Is_Taken() {
-            Game tic = new Game(5, 5);
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 1, 4);
             bool isEmpty = tic.FieldIsTaken(1, 4);
             Assert.IsTrue(isEmpty);
@@ -16,7 +16,7 @@ namespace UnitTests {
 
         [TestMethod]
         public void FieldIsTaken_Should_Return_False_When_The_Field_Is_Not_Taken() {
-            Game tic = new Game(5, 5);
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 1, 4);
             bool isEmpty = tic.FieldIsTaken(3, 3);
             Assert.IsFalse(isEmpty);
@@ -24,27 +24,27 @@ namespace UnitTests {
 
         [TestMethod]
         public void PlaceChar_Should_Return_True_When_Char_Is_Placed_In_Empty_Field() {
-            Game tic = new Game(5, 5);
+            GameBoard tic = new GameBoard(5, 5);
             Assert.IsTrue(tic.PlaceChar(Piece.X, 1, 4));
         }
 
         [TestMethod]
         public void PlaceChar_Should_Place_Char_In_Correct_Location() {
-            Game tic = new Game(5, 5);
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 1, 4);
             Assert.IsTrue(tic.Board[1, 4].Equals(Piece.X));
         }
 
         [TestMethod]
         public void PlaceChar_Should_Return_False_When_Field_Is_Taken() {
-            Game tic = new Game(5, 5);
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.O, 1, 4);
             Assert.IsFalse(tic.PlaceChar(Piece.X, 1, 4));
         }
 
         [TestMethod]
-        public void CheckVerticals_Should_Return_True_When_There_Are_Consecutive_Chars_In_Same_Column() {
-            Game tic = new Game(5, 5);
+        public void CheckVerticals_Should_Return_True_When_There_Is_A_Win() {
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 0, 0);
             tic.PlaceChar(Piece.X, 0, 1);
             tic.PlaceChar(Piece.X, 0, 2);
@@ -60,8 +60,25 @@ namespace UnitTests {
         }
 
         [TestMethod]
-        public void CheckHorizontals_Should_Return_True_When_There_Are_Consecutive_Charss() {
-            Game tic = new Game(5, 5);
+        public void CheckVerticals_Should_Return_False_When_There_Is_No_Win() {
+            GameBoard tic = new GameBoard(5, 5);
+            tic.PlaceChar(Piece.X, 0, 0);
+            tic.PlaceChar(Piece.X, 0, 1);
+            tic.PlaceChar(Piece.X, 0, 2);
+            tic.PlaceChar(Piece.X, 0, 3);
+            // tic.PlaceChar(Piece.X, 0, 4); this should remain commented out
+            Assert.IsFalse(tic.CheckVerticals(Piece.X));
+            // tic.PlaceChar(Piece.O, 4, 0); this should remain commented out
+            tic.PlaceChar(Piece.O, 4, 1);
+            tic.PlaceChar(Piece.O, 4, 2);
+            tic.PlaceChar(Piece.O, 4, 3);
+            tic.PlaceChar(Piece.O, 4, 4);
+            Assert.IsFalse(tic.CheckVerticals(Piece.O));
+        }
+
+        [TestMethod]
+        public void CheckHorizontals_Should_Return_True_When_There_Is_A_Win() {
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 0, 0);
             tic.PlaceChar(Piece.X, 1, 0);
             tic.PlaceChar(Piece.X, 2, 0);
@@ -77,8 +94,25 @@ namespace UnitTests {
         }
 
         [TestMethod]
-        public void CheckDiagonalDownRight_Should_Return_True_When_There_Are_Consecutive_Chars() {
-            Game tac = new Game(5, 5);
+        public void CheckHorizontals_Should_Return_False_When_There_Is_No_Win() {
+            GameBoard tic = new GameBoard(5, 5);
+            tic.PlaceChar(Piece.X, 0, 0);
+            tic.PlaceChar(Piece.X, 1, 0);
+            tic.PlaceChar(Piece.X, 2, 0); 
+            tic.PlaceChar(Piece.X, 3, 0);
+            //tic.PlaceChar(Piece.X, 4, 0); this should remain commented out
+            Assert.IsFalse(tic.CheckHorizontals(Piece.X));
+            // tic.PlaceChar(Piece.O, 0, 4); this should remain commented out
+            tic.PlaceChar(Piece.O, 1, 4);
+            tic.PlaceChar(Piece.O, 2, 4); 
+            tic.PlaceChar(Piece.O, 3, 4);
+            tic.PlaceChar(Piece.O, 4, 4);
+            Assert.IsFalse(tic.CheckHorizontals(Piece.O));
+        }
+
+        [TestMethod]
+        public void CheckDiagonalDownRight_Should_Return_True_When_There_Is_A_Win() {
+            GameBoard tac = new GameBoard(5, 5);
             tac.PlaceChar(Piece.O, 0, 0);
             tac.PlaceChar(Piece.O, 1, 1);
             tac.PlaceChar(Piece.O, 2, 2);
@@ -88,8 +122,19 @@ namespace UnitTests {
         }
 
         [TestMethod]
-        public void CheckDiagonalUpRight_Should_Return_True_When_There_Are_Consecutive_Chars() {
-            Game tic = new Game(5, 5);
+        public void CheckDiagonalDownRight_Should_Return_False_When_There_Are_Is_No_Win() {
+            GameBoard tac = new GameBoard(5, 5);
+            tac.PlaceChar(Piece.O, 0, 0);
+            tac.PlaceChar(Piece.O, 1, 1);
+            // tac.PlaceChar(Piece.O, 2, 2); this should remain commented out
+            tac.PlaceChar(Piece.O, 3, 3);
+            tac.PlaceChar(Piece.O, 4, 4);
+            Assert.IsFalse(tac.CheckDiagonalDownRight(Piece.O));
+        }
+
+        [TestMethod]
+        public void CheckDiagonalUpRight_Should_Return_True_When_There_Is_A_Win() {
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 0, 4);
             tic.PlaceChar(Piece.X, 1, 3);
             tic.PlaceChar(Piece.X, 2, 2);
@@ -99,17 +144,8 @@ namespace UnitTests {
         }
 
         [TestMethod]
-        public void GetLengths() {
-            Game tic = new Game(5, 4);
-            tic.PlaceChar(Piece.X, 0, 0);
-            Assert.IsTrue(tic.Board[0, 0].Equals(Piece.X));
-            tic.PlaceChar(Piece.O, 0, 1);
-            Assert.IsTrue(tic.Board[0, 1].Equals(Piece.O));
-        }
-
-        [TestMethod]
-        public void CheckDiagonalUpRight_Should_Return_False_When_There_Are_No_Consecutive_Chars() {
-            Game tic = new Game(5, 5);
+        public void CheckDiagonalUpRight_Should_Return_False_When_There_Are_Is_No_Win() {
+            GameBoard tic = new GameBoard(5, 5);
             tic.PlaceChar(Piece.X, 0, 4);
             tic.PlaceChar(Piece.X, 1, 3);
             //tic.PlaceChar(Piece.X, 2, 2); this should remain commented out
@@ -119,14 +155,16 @@ namespace UnitTests {
         }
 
         [TestMethod]
-        public void CheckDiagonalDownRight_Should_Return_False_When_There_Are_No_Consecutive_Chars() {
-            Game tac = new Game(5, 5);
-            tac.PlaceChar(Piece.O, 0, 0);
-            tac.PlaceChar(Piece.O, 1, 1);
-            // tac.PlaceChar(Piece.O, 2, 2); this should remain commented out
-            tac.PlaceChar(Piece.O, 3, 3);
-            tac.PlaceChar(Piece.O, 4, 4);
-            Assert.IsFalse(tac.CheckDiagonalDownRight(Piece.O));
+        public void GetLengths() {
+            GameBoard tic = new GameBoard(5, 4);
+            tic.PlaceChar(Piece.X, 0, 0);
+            Assert.IsTrue(tic.Board[0, 0].Equals(Piece.X));
+            tic.PlaceChar(Piece.O, 0, 1);
+            Assert.IsTrue(tic.Board[0, 1].Equals(Piece.O));
         }
+
+        
+
+        
     }
 }
