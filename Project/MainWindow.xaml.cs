64618a3d38c78e9e60ca91d7db8e079a1c9238a4
icygub <jchubbs95@gmail.com>
2017-12-07 16:40:13 -0700

    game is playable

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -12,16 +12,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Project.GameBoard;
 
 namespace Project {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private ViewModel viewModel;
+        private Piece currentPiece;
+
         public MainWindow() {
             InitializeComponent();
+            viewModel = new ViewModel();
+            currentPiece = Piece.X;
+        }
 
-            
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            int col = Int32.Parse(((Button)sender).Name.Substring(1, 1));
+            int row = Int32.Parse(((Button)sender).Name.Substring(2, 1));
+            bool piecePlayed = viewModel.PlayPiece(col, row); // if there is no win, then piecePlayed will be true
+            if(piecePlayed) {
+                ((Button)sender).Content = Enum.GetName(typeof(Piece), currentPiece);
+                if (! viewModel.CheckWin()) {
+                    currentPiece = (currentPiece == Piece.X) ? Piece.O : Piece.X;
+                } else {
+                    MessageBox.Show(Enum.GetName(typeof(Piece), currentPiece) + " has WON!");
+                }
+            } else if(viewModel.CheckWin()) {
+                MessageBox.Show(Enum.GetName(typeof(Piece), currentPiece) + " has WON!");
+            }
+
+            //Enum.GetName(typeof(Piece), Piece.X);
+            //MessageBox.Show(col + " " + row);
         }
     }
 }
--- Project/ViewModel.cs
+++ Project/ViewModel.cs
@@ -13,18 +13,27 @@ namespace Project {
             Game = new Game(true, true);
         }
 
-        public Game Game { get => _game; set => _game = value; }
+        public Game Game { get => _game; set => _game = value; } 
 
         public bool PlayPiece(int col, int row) {
-            bool win = false;
+            //bool win = false;
+            if(CheckWin()) {
+                return false;
+            }
             bool success = Game.PlayPieceNoAlternate(col, row);
-            if(success) {
-                win = Game.CheckWin();
-                if (!win) { // when win == false
-                    Game.AlternatePlayer();
-                }
-            }    
-            return win;
+            if (success && !CheckWin()) {
+                Game.AlternatePlayer();
+            }
+            //win = Game.CheckWin();
+            //if (!win) { // when win == false
+            //    Game.AlternatePlayer();
+            //}
+            //return win;
+            return success;
+        }
+
+        public bool CheckWin() {
+            return _game.CheckWin();
         }
 
     }
